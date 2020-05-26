7db43b80e22132b0f40da9261a8d4c63b98c777f
SonicEraZoR <33545675+SonicEraZoR@users.noreply.github.com>
2020-05-26 19:10:43 +0700

    Fix for mod crashes while Steam if running
    Fix from Valve developer Wiki: https://developer.valvesoftware.com/wiki/Setup_mod_on_steam#Problems_with_running_your_mod

--- sp/src/public/steam/steam_api.h
+++ sp/src/public/steam/steam_api.h
@@ -547,7 +547,7 @@ inline bool CSteamAPIContext::Init()
 	{
 		return false;
 	}
-
+/*
 	m_pSteamMusicRemote = SteamClient()->GetISteamMusicRemote( hSteamUser, hSteamPipe, STEAMMUSICREMOTE_INTERFACE_VERSION );
 	if ( !m_pSteamMusicRemote )
 	{
@@ -559,7 +559,7 @@ inline bool CSteamAPIContext::Init()
 	{
 		return false;
 	}
-
+*/
 #ifdef _PS3
 	m_pSteamPS3OverlayRender = SteamClient()->GetISteamPS3OverlayRender();
 #endif
