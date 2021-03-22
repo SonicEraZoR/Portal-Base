593083a1511c548bff8907d682d5a5f0871eaadc
SonicEraZoR <33545675+SonicEraZoR@users.noreply.github.com>
2021-03-23 00:50:29 +0700

    Update basecombatweapon_shared.cpp
    Fixes NPCs not firing bullets (I HAD TO DEBUG FOR MONTHS TO FIND THIS AND FIX IT)

--- sp/src/game/shared/basecombatweapon_shared.cpp
+++ sp/src/game/shared/basecombatweapon_shared.cpp
@@ -1009,7 +1009,8 @@ void CBaseCombatWeapon::SetActivity( Activity act, float duration )
 { 
 	//Adrian: Oh man...
 #if !defined( CLIENT_DLL ) && (defined( HL2MP ) || defined( PORTAL ))
-	SetModel( GetWorldModel() );
+	if (GetOwner()->IsPlayer())
+		SetModel(GetWorldModel());
 #endif
 	
 	int sequence = SelectWeightedSequence( act ); 
@@ -1020,7 +1021,8 @@ void CBaseCombatWeapon::SetActivity( Activity act, float duration )
 
 	//Adrian: Oh man again...
 #if !defined( CLIENT_DLL ) && (defined( HL2MP ) || defined( PORTAL ))
-	SetModel( GetViewModel() );
+	if (GetOwner()->IsPlayer())
+		SetModel(GetViewModel());
 #endif
 
 	if ( sequence != ACTIVITY_NOT_AVAILABLE )
