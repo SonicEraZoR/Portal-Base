035845b10cf521ff9a0de1d601ddde4f3b7a1a64
SonicEraZoR <33545675+SonicEraZoR@users.noreply.github.com>
2021-03-23 00:47:09 +0700

    Separated weapon_pistol.cpp into .cpp and .h file
    Needed to separate this for debugging and don't need them separated anymore but why not leave them separated

--- sp/src/game/server/hl2/weapon_pistol.cpp
+++ sp/src/game/server/hl2/weapon_pistol.cpp
@@ -11,6 +11,7 @@
 #include "basecombatcharacter.h"
 #include "ai_basenpc.h"
 #include "player.h"
+#include "weapon_pistol.h"
 #include "gamerules.h"
 #include "in_buttons.h"
 #include "soundent.h"
@@ -21,97 +22,11 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-#define	PISTOL_FASTEST_REFIRE_TIME		0.1f
-#define	PISTOL_FASTEST_DRY_REFIRE_TIME	0.2f
-
-#define	PISTOL_ACCURACY_SHOT_PENALTY_TIME		0.2f	// Applied amount of time each shot adds to the time we must recover from
-#define	PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME	1.5f	// Maximum penalty to deal out
-
 ConVar	pistol_use_new_accuracy( "pistol_use_new_accuracy", "1" );
 
 //-----------------------------------------------------------------------------
 // CWeaponPistol
 //-----------------------------------------------------------------------------
-
-class CWeaponPistol : public CBaseHLCombatWeapon
-{
-	DECLARE_DATADESC();
-
-public:
-	DECLARE_CLASS( CWeaponPistol, CBaseHLCombatWeapon );
-
-	CWeaponPistol(void);
-
-	DECLARE_SERVERCLASS();
-
-	void	Precache( void );
-	void	ItemPostFrame( void );
-	void	ItemPreFrame( void );
-	void	ItemBusyFrame( void );
-	void	PrimaryAttack( void );
-	void	AddViewKick( void );
-	void	DryFire( void );
-	void	Operator_HandleAnimEvent( animevent_t *pEvent, CBaseCombatCharacter *pOperator );
-
-	void	UpdatePenaltyTime( void );
-
-	int		CapabilitiesGet( void ) { return bits_CAP_WEAPON_RANGE_ATTACK1; }
-	Activity	GetPrimaryAttackActivity( void );
-
-	virtual bool Reload( void );
-
-	virtual const Vector& GetBulletSpread( void )
-	{		
-		// Handle NPCs first
-		static Vector npcCone = VECTOR_CONE_5DEGREES;
-		if ( GetOwner() && GetOwner()->IsNPC() )
-			return npcCone;
-			
-		static Vector cone;
-
-		if ( pistol_use_new_accuracy.GetBool() )
-		{
-			float ramp = RemapValClamped(	m_flAccuracyPenalty, 
-											0.0f, 
-											PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME, 
-											0.0f, 
-											1.0f ); 
-
-			// We lerp from very accurate to inaccurate over time
-			VectorLerp( VECTOR_CONE_1DEGREES, VECTOR_CONE_6DEGREES, ramp, cone );
-		}
-		else
-		{
-			// Old value
-			cone = VECTOR_CONE_4DEGREES;
-		}
-
-		return cone;
-	}
-	
-	virtual int	GetMinBurst() 
-	{ 
-		return 1; 
-	}
-
-	virtual int	GetMaxBurst() 
-	{ 
-		return 3; 
-	}
-
-	virtual float GetFireRate( void ) 
-	{
-		return 0.5f; 
-	}
-
-	DECLARE_ACTTABLE();
-
-private:
-	float	m_flSoonestPrimaryAttack;
-	float	m_flLastAttackTime;
-	float	m_flAccuracyPenalty;
-	int		m_nNumShotsFired;
-};
 
 
 IMPLEMENT_SERVERCLASS_ST(CWeaponPistol, DT_WeaponPistol)
@@ -149,6 +64,38 @@ acttable_t	CWeaponPistol::m_acttable[] =
 
 
 IMPLEMENT_ACTTABLE( CWeaponPistol );
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+const Vector& CWeaponPistol::GetBulletSpread(void)
+{
+	// Handle NPCs first
+	static Vector npcCone = VECTOR_CONE_5DEGREES;
+	if (GetOwner() && GetOwner()->IsNPC())
+		return npcCone;
+
+	static Vector cone;
+
+	if (pistol_use_new_accuracy.GetBool())
+	{
+		float ramp = RemapValClamped(m_flAccuracyPenalty,
+			0.0f,
+			PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME,
+			0.0f,
+			1.0f);
+
+		// We lerp from very accurate to inaccurate over time
+		VectorLerp(VECTOR_CONE_1DEGREES, VECTOR_CONE_6DEGREES, ramp, cone);
+	}
+	else
+	{
+		// Old value
+		cone = VECTOR_CONE_4DEGREES;
+	}
+
+	return cone;
+}
 
 //-----------------------------------------------------------------------------
 // Purpose: Constructor
---
+++ sp/src/game/server/hl2/weapon_pistol.h
@@ -0,0 +1,79 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose:		Pistol - hand gun
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#ifndef WEAPON_PISTOL_H
+#define WEAPON_PISTOL_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include "basehlcombatweapon.h"
+
+#define	PISTOL_FASTEST_REFIRE_TIME		0.1f
+#define	PISTOL_FASTEST_DRY_REFIRE_TIME	0.2f
+
+#define	PISTOL_ACCURACY_SHOT_PENALTY_TIME		0.2f	// Applied amount of time each shot adds to the time we must recover from
+#define	PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME	1.5f	// Maximum penalty to deal out
+
+//-----------------------------------------------------------------------------
+// CWeaponPistol
+//-----------------------------------------------------------------------------
+
+class CWeaponPistol : public CBaseHLCombatWeapon
+{
+	DECLARE_DATADESC();
+
+public:
+	DECLARE_CLASS(CWeaponPistol, CBaseHLCombatWeapon);
+
+	CWeaponPistol(void);
+
+	DECLARE_SERVERCLASS();
+
+	void	Precache(void);
+	void	ItemPostFrame(void);
+	void	ItemPreFrame(void);
+	void	ItemBusyFrame(void);
+	void	PrimaryAttack(void);
+	void	AddViewKick(void);
+	void	DryFire(void);
+	void	Operator_HandleAnimEvent(animevent_t *pEvent, CBaseCombatCharacter *pOperator);
+
+	void	UpdatePenaltyTime(void);
+
+	int		CapabilitiesGet(void) { return bits_CAP_WEAPON_RANGE_ATTACK1; }
+	Activity	GetPrimaryAttackActivity(void);
+
+	virtual bool Reload(void);
+
+	virtual const Vector& GetBulletSpread(void);
+
+	virtual int	GetMinBurst()
+	{
+		return 1;
+	}
+
+	virtual int	GetMaxBurst()
+	{
+		return 3;
+	}
+
+	virtual float GetFireRate(void)
+	{
+		return 0.5f;
+	}
+
+	DECLARE_ACTTABLE();
+
+private:
+	float	m_flSoonestPrimaryAttack;
+	float	m_flLastAttackTime;
+	float	m_flAccuracyPenalty;
+	int		m_nNumShotsFired;
+};
+
+#endif // WEAPON_PISTOL_H
