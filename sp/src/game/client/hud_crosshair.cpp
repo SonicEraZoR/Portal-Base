05822539a906500ca94dc784e03bedacf40b403a
SonicEraZoR <33545675+SonicEraZoR@users.noreply.github.com>
2021-03-23 17:20:14 +0700

    Revert changes

--- sp/src/game/client/hud_crosshair.cpp
+++ sp/src/game/client/hud_crosshair.cpp
@@ -93,6 +93,12 @@ bool CHudCrosshair::ShouldDraw( void )
 	if ( pWeapon && !pWeapon->ShouldDrawCrosshair() )
 		return false;
 
+#ifdef PORTAL
+	C_Portal_Player *portalPlayer = ToPortalPlayer(pPlayer);
+	if ( portalPlayer && portalPlayer->IsSuppressingCrosshair() )
+		return false;
+#endif // PORTAL
+
 	/* disabled to avoid assuming it's an HL2 player.
 	// suppress crosshair in zoom.
 	if ( pPlayer->m_HL2Local.m_bZooming )
