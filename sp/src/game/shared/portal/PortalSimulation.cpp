3b799a142cf762ec79245e47177ff2e6ddbea0e0
BusterBunny <33545675+SonicEraZoR@users.noreply.github.com>
2022-04-12 00:17:31 +0700

    Revert "Update PortalSimulation.cpp"
    This reverts commit 5cfaf2aabe8ae6d78d2607d71375af2e10090cb0.

--- sp/src/game/shared/portal/PortalSimulation.cpp
+++ sp/src/game/shared/portal/PortalSimulation.cpp
@@ -1,4 +1,4 @@
-//========= Copyright Â© 1996-2006, Valve Corporation, All rights reserved. ============//
+//========= Copyright © 1996-2006, Valve Corporation, All rights reserved. ============//
 //
 // Purpose: 
 //
@@ -7,7 +7,7 @@
 
 
 #include "cbase.h"
-#include "PortalSimulation.h"
+#include "portalsimulation.h"
 #include "vphysics_interface.h"
 #include "physics.h"
 #include "portal_shareddefs.h"
