426b08b90415e4afb44c355523163a8dc0e3457c
SonicEraZoR <33545675+SonicEraZoR@users.noreply.github.com>
2022-06-18 08:20:59 +0700

    Revert "Auxiliary commit to revert individual files from cd20e8eac321286170d49297ef880dd343005737"
    This reverts commit 28689716951ba7bca66ccb6bcea37b8d904c1022.
    
    Apparently I need to revert this commit to fix reload sound playing twice, of course it brings back old bugs, so that needs to be taken care of

--- sp/src/game/client/hl2/c_basehlcombatweapon.h
+++ sp/src/game/client/hl2/c_basehlcombatweapon.h
@@ -4,7 +4,7 @@
 //
 //=============================================================================//
 
-#include "weapon_portalbasecombatweapon.h"
+#include "basehlcombatweapon_shared.h"
 
 #ifndef C_BASEHLCOMBATWEAPON_H
 #define C_BASEHLCOMBATWEAPON_H
@@ -12,10 +12,10 @@
 #pragma once
 #endif
 
-class C_HLMachineGun : public C_BasePortalCombatWeapon
+class C_HLMachineGun : public C_BaseHLCombatWeapon
 {
 public:
-	DECLARE_CLASS(C_HLMachineGun, C_BasePortalCombatWeapon);
+	DECLARE_CLASS( C_HLMachineGun, C_BaseHLCombatWeapon );
 	DECLARE_CLIENTCLASS();
 };
 
@@ -26,10 +26,10 @@ public:
 	DECLARE_CLIENTCLASS();
 };
 
-class C_BaseHLBludgeonWeapon : public C_BasePortalCombatWeapon
+class C_BaseHLBludgeonWeapon : public C_BaseHLCombatWeapon
 {
 public:
-	DECLARE_CLASS(C_BaseHLBludgeonWeapon, C_BasePortalCombatWeapon);
+	DECLARE_CLASS( C_BaseHLBludgeonWeapon, C_BaseHLCombatWeapon );
 	DECLARE_CLIENTCLASS();
 };
 
